5135bdb1aa220cf34773b4209653b6be58609527
young <2028624850@qq.com>
2021-10-07 14:42:47 +0800

    Create git.css; Modified index.html:h1 and imoort git.css

---
+++ cmake-build-debug/CMakeFiles/Git_Example.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Git_Example.dir/index.html.c.obj"
+  "Git_Example.exe"
+  "Git_Example.exe.manifest"
+  "Git_Example.pdb"
+  "libGit_Example.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Git_Example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
